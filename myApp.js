69f1456ab40a730851cd08eb34c80e214ba3ca84
Got-crypto <pecpalmplay@gmail.com>
2023-07-15 07:51:48 +0200

    served 'Hello express' string

--- myApp.js
+++ myApp.js
@@ -1,36 +1,9 @@
 let express = require('express');
 let app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/', (req, res) => {
+    res.send("Hello express")
+})
 
 
 
